fb44f64afd50a2cf0aa2f2fac3d9b84ddf6e85b9
김진영 <jinyoung01099@gmail.com>
2023-01-29 21:15:13 +0900

    [Add] react-redux를 통해서 카운팅 앱 구현

--- src/App.tsx
+++ src/App.tsx
@@ -1,48 +1,66 @@
-import React, { useState } from "react";
+import { createStore } from "redux";
+import { Provider, useSelector, useDispatch } from "react-redux";
 import "./App.css";
 
-interface LeftType {
+interface State {
   number: number;
 }
 
-interface RightType {
-  number: number;
-  onIncrease: () => void;
+interface Action {
+  type: string;
 }
+
+const reducer = (currentState: any, action: Action) => {
+  const newState = { ...currentState };
+  if (currentState === undefined) {
+    return { number: 1 };
+  }
+  if (action.type === "PLUS") {
+    newState.number++;
+  }
+  console.log(currentState);
+  return newState;
+};
+
+const store = createStore(reducer);
 
 const App = () => {
-  const [number, setNumber] = useState(1);
-
   return (
     <div id="container">
-      <h1>Root : {number}</h1>
+      <h1>Root </h1>
       <div id="grid">
-        <Left1 number={number} />
-        <Right1 number={number} onIncrease={() => setNumber(number + 1)} />
+        <Provider store={store}>
+          <Left1 />
+          <Right1 />
+        </Provider>
       </div>
     </div>
   );
 };
 
-const Left1: React.FC<LeftType> = ({ number }) => {
+const Left1 = () => {
   return (
     <div>
-      <h1>Left1 : {number}</h1>
-      <Left2 number={number} />
+      <h1>Left1 :</h1>
+      <Left2 />
     </div>
   );
 };
 
-const Left2: React.FC<LeftType> = ({ number }) => {
+const Left2 = () => {
+  const number = useSelector((state: State) => state.number);
+
   return (
     <div>
       <h1>Left2 : {number}</h1>
-      <Left3 number={number} />
+      <Left3 />
     </div>
   );
 };
 
-const Left3: React.FC<LeftType> = ({ number }) => {
+const Left3 = () => {
+  const number = useSelector((state: State) => state.number);
+
   return (
     <div>
       <h1>Left3 : {number}</h1>
@@ -50,31 +68,32 @@ const Left3: React.FC<LeftType> = ({ number }) => {
   );
 };
 
-const Right1: React.FC<RightType> = ({ number, onIncrease }) => {
+const Right1 = () => {
   return (
     <div>
-      <h1>Right1 : {number}</h1>
-      <Right2 number={number} onIncrease={onIncrease} />
+      <h1>Right1 : </h1>
+      <Right2 />
     </div>
   );
 };
 
-const Right2: React.FC<RightType> = ({ number, onIncrease }) => {
+const Right2 = () => {
   return (
     <div>
-      <h1>Right2 : {number}</h1>
-      <Right3 number={number} onIncrease={onIncrease} />
+      <h1>Right2 : </h1>
+      <Right3 />
     </div>
   );
 };
 
-const Right3: React.FC<RightType> = ({ number, onIncrease }) => {
+const Right3 = () => {
+  const dispatch = useDispatch();
   return (
     <div>
-      <h1>Right3 : {number}</h1>
+      <h1>Right3 : </h1>
       <button
         onClick={() => {
-          onIncrease();
+          dispatch({ type: "PLUS" });
         }}
       >
         +
